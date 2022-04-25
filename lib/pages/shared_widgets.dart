eedc4c1bdf1194c3972d541ec5a57b7f95a46004
Gilberto Junior <gagjunior@hotmail.com>
2022-04-25 00:45:23 -0300

    Flexibilizar componente

--- lib/pages/shared_widgets.dart
+++ lib/pages/shared_widgets.dart
@@ -1,36 +1,73 @@
 import 'package:fluent_ui/fluent_ui.dart';
 
-PageHeader headerPage(IconData icon, String titulo, String subtitulo) {
-  return PageHeader(
-    title: Padding(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            titulo,
-            style: const TextStyle(
-              fontSize: 26,
-              color: Color.fromARGB(255, 10, 34, 255),
+Widget titlePageHeader(IconData icon, String titulo, String subtitulo) {
+  return Padding(
+    child: Row(
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              titulo,
+              style: const TextStyle(
+                fontSize: 26,
+                color: Color.fromARGB(255, 10, 34, 255),
+              ),
             ),
-          ),
-          Text(
-            subtitulo,
-            style: const TextStyle(
-              fontSize: 14,
-              fontWeight: FontWeight.normal,
+            Text(
+              subtitulo,
+              style: const TextStyle(
+                fontSize: 14,
+                fontWeight: FontWeight.normal,
+              ),
             ),
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Icon(
+            icon,
+            size: 26,
+            color: const Color.fromARGB(255, 10, 34, 255),
           ),
-        ],
-      ),
-      padding: const EdgeInsets.all(8.0),
+        ),
+      ],
     ),
-    leading: Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Icon(
-        icon,
-        size: 26,
-        color: const Color.fromARGB(255, 10, 34, 255),
-      ),
+    padding: const EdgeInsets.all(8.0),
+  );
+}
+
+Widget buttonMenuPage(String titulo, String subtitulo) {
+  return Button(
+    style: ButtonStyle(
+      elevation: ButtonState.all(4),
     ),
+    child: Row(
+      children: [
+        Icon(
+          FluentIcons.group,
+          size: 30,
+          semanticLabel: titulo,
+          textDirection: TextDirection.ltr,
+        ),
+        const SizedBox(width: 10),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              titulo,
+              style: const TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Text(
+              subtitulo,
+            ),
+          ],
+        ),
+      ],
+    ),
+    onPressed: () {},
   );
 }
